0fa31b1f9764bf55899fcdcc4ff55b3fb4a344bc
p-mostovoi <145673281+p-mostovoi@users.noreply.github.com>
2024-09-18 17:01:31 +0200

    added API stub

--- api/main.py
+++ api/main.py
@@ -16,4 +16,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=30000)
+    uvicorn.run(app, host="127.0.0.1", port=30000)
