3d164e514084689b416b32564550a25eb4c7e665
Pavel Mostovoy <pavelmostovoy@gmail.com>
2024-10-01 17:22:07 +0200

    adding SOG display rework

--- src/elements/controls.py
+++ src/elements/controls.py
@@ -8,6 +8,7 @@ from flet_core import TimePickerEntryMode, DatePicker
 from sqlalchemy.orm import Session
 
 from .utils.db_tools import Sailboat, engine
+
 reference_button = ft.Ref[ft.ElevatedButton]()
 
 data_container = ft.Ref[ft.Container]()
@@ -28,16 +29,21 @@ def polyline_update(polyline, e):
     return polyline
 
 
-def coords_replace(received_coordinated, range, actual_coordinates):
-    # need to be reworked
-    actual_coordinates.clear()
-    for coord in received_coordinated[0:-1]:
-        # if coord.time > datetime.strptime("22/09/2024::10:08:00", '%d/%m/%Y::%H:%M:%S'):
-        prepared_coord = f_map.MapLatitudeLongitude(coord.lat, coord.lon)
-        actual_coordinates.append(prepared_coord)
+def coords_prepare(received_coordinated: list, limit: datetime.datetime):
+    tail_length = datetime.timedelta(minutes=10)
+    start_time = limit - tail_length
+    prepared_coordinates = []
+    last_sog = 0.0
+    for coord in received_coordinated:
+        if start_time < coord[0] <= limit:
+            prepared_coord = f_map.MapLatitudeLongitude(coord[1], coord[2])
+            prepared_coordinates.append(prepared_coord)
+            last_sog = coord[3]
+    return prepared_coordinates, round(last_sog,2)
 
 
 def manage_data_container(e):
+    tracks = e.page.session.get("tracks")
     order = int(e.control.data)
     containers = e.page.overlay[0].controls[1].controls[0].controls
     polyline = e.page.controls[0].layers[1].polylines[order]
@@ -48,32 +54,39 @@ def manage_data_container(e):
         received_all_coordinated = user.children
         received_coordinates = []
         # 2024-09-23::00:00:00
-        start_time = datetime.datetime.strptime(f"{e.page.session.get("date").date()}::{e.page.session.get("start_time")}",'%Y-%m-%d::%H:%M:%S')
-        print(start_time)
+        selected_date = e.page.session.get("date")
         for coord in received_all_coordinated:
-            if coord.time.date()==e.page.session.get("date").date():
-                received_coordinates.append(coord)
+            if coord.time.date() == selected_date.date():
+                received_coordinates.append((coord.time, coord.lat, coord.lon,coord.sog))
+        if received_coordinates:
+            if received_coordinates[0][0] < e.page.session.get("start_time"):
+                e.page.session.set("start_time", received_coordinates[0][0])
+            if received_coordinates[-1][0] > e.page.session.get("end_time"):
+                e.page.session.set("end_time", received_coordinates[-1][0])
 
     if e.control.value:
         for container in containers:
-            if container.content.value == identifier:
+            if container.content.data == identifier:
                 e.page.update()
                 return
-
-        containers.append(MonitoringContainer(content=ft.Text(identifier),
-                                              bgcolor=e.control.active_color))
+        container = MonitoringContainer(content=ft.Text("SOG", size=30),
+                                              bgcolor=e.control.active_color)
+        container.content.data = identifier
+        containers.append(container)
         polyline_update(polyline, e)
 
-        coords_replace(received_coordinates, 100, actual_coordinates)
 
-        e.page.update()
+        tracks[identifier] = (order, received_coordinates)
+
+
 
     else:
+        tracks.pop(identifier)
         for i, container in enumerate(containers):
-            if container.content.value == identifier:
+            if container.content.data == identifier:
                 actual_coordinates.clear()
                 containers.pop(i)
-                e.page.update()
+    e.page.update()
 
 
 class MyCheckboxes(ft.Row):
@@ -120,12 +133,37 @@ class MyCheckbox(ft.Checkbox):
 
 class MySlider(ft.Slider):
 
-    @staticmethod
-    def slider_change(e):
-        e.page.pubsub.send_all(Message("Slider", int(e.control.value)))
+    def slider_change(self, e):
+        start_point = e.page.session.get("start_time").timestamp()
+        end_point = e.page.session.get("end_time").timestamp()
+
+        if self.max != end_point:
+            if self.value >= end_point:
+                self.value = end_point
+            self.max = end_point
+
+        if self.min != start_point:
+            if self.value <= start_point:
+                self.value = start_point
+            self.min = start_point
+
+        hashmap_for_containers = {}
+        for i, container in enumerate(e.page.overlay[0].controls[1].controls[0].controls):
+            hashmap_for_containers[container.content.data] = i
+
+        for owner, track in e.page.session.get("tracks").items():
+            container = e.page.overlay[0].controls[1].controls[0].controls[hashmap_for_containers[owner]]
+            order = track[0]
+            coords = track[1]
+            current_coordinates = e.page.controls[0].layers[1].polylines[order].coordinates
+            actual_coords, container.content.value  = coords_prepare(coords, datetime.datetime.fromtimestamp(self.value))
+            current_coordinates.clear()
+            current_coordinates.extend(actual_coords)
+
+        e.page.update()
 
     def __init__(self):
-        super().__init__(min=0, max=100)
+        super().__init__(min=datetime.datetime.now().timestamp() - 1000, max=datetime.datetime.now().timestamp())
 
     def build(self):
         self.height = 50,
@@ -152,38 +190,41 @@ class MonitoringContainer(ft.Container):
 class TimeSelector(ft.TimePicker):
 
     @staticmethod
-    def handle_change_start(e):
-        print(f"start time {e.control.value}")
-        print(f"data {e.page.session.get("date")}")
+    def handle_change(e):
+        start_time = datetime.datetime.strptime(
+            f"{e.page.session.get("date").date()}::{e.control.value}", '%Y-%m-%d::%H:%M:%S')
+        end_time = start_time + datetime.timedelta(hours=8)
+        e.page.session.set("start_time", start_time)
+        e.page.session.set("end_time", end_time)
+        e.page.update()
 
-    @staticmethod
-    def handle_change_end(e):
-        print(f"end time {e.control.value}")
 
-    def __init__(self, start=False):
+
+    def __init__(self):
         super().__init__()
-        if start:
-            self.value = "00:00:00"
-            self.on_change = self.handle_change_start
-        else:
-            # should be clarified after usage
-            self.value = "23:59:59"
-            self.on_change = self.handle_change_end
+        self.value = "09:00:00"
+        self.on_change = self.handle_change
         self.time_picker_entry_mode = TimePickerEntryMode.DIAL_ONLY
         self.confirm_text = "Confirm"
         self.help_text = "Select time"
 
 
+
+
 class DateSelector(DatePicker):
 
-
-    def handle_change(self,e):
+    def handle_change(self, e):
         reference_button.current.text = f"{e.control.value.date()}"
-        e.page.session.set("date",e.control.value)
+        e.page.session.set("date", e.control.value)
+        start_time = datetime.datetime.strptime(
+            f"{e.control.value.date()}::{e.page.session.get('start_time').time()}", '%Y-%m-%d::%H:%M:%S')
+        end_time = datetime.datetime.strptime(
+            f"{e.control.value.date()}::{e.page.session.get('end_time').time()}", '%Y-%m-%d::%H:%M:%S')
+        e.page.session.set("start_time", start_time)
+        e.page.session.set("end_time", end_time)
         e.page.update()
 
     def __init__(self):
         super().__init__()
         self.first_date = datetime.datetime(year=2024, month=9, day=1)
         self.on_change = self.handle_change
-
--- src/main.py
+++ src/main.py
@@ -9,8 +9,11 @@ from elements.controls import MySlider, MyCheckboxes, TimeSelector, DateSelector
 
 def main(page: ft.Page):
     page.session.set("date", datetime.datetime.now())
-    page.session.set("start_time", "00:00:00")
-    page.session.set("end_time", "23:59:59")
+    page.session.set("start_time", datetime.datetime.strptime(
+            f"{page.session.get("date").date()}::11:59:00", '%Y-%m-%d::%H:%M:%S'))
+    page.session.set("end_time", datetime.datetime.strptime(
+            f"{page.session.get("date").date()}::12:00:00", '%Y-%m-%d::%H:%M:%S'))
+    page.session.set("tracks", {})
 
     def on_broadcast_message(message):
         # Need to be implemented
@@ -29,18 +32,13 @@ def main(page: ft.Page):
     start_btn = ft.ElevatedButton(
         "Start time",
         icon=ft.icons.START,
-        on_click=lambda _: page.open(TimeSelector(start=True)),
-    )
-
-    end_btn = ft.ElevatedButton(
-        "End time",
-        icon=ft.icons.CLOSE,
         on_click=lambda _: page.open(TimeSelector()),
     )
 
+
     def main_struct():
         return ft.Column(
-            controls=[ft.Row(controls=[MyCheckboxes(), date_selector, start_btn, end_btn]),
+            controls=[ft.Row(controls=[MyCheckboxes(), date_selector, start_btn]),
                       ft.Row(controls=[ft.Column(
                           # controls=[],
                           alignment=ft.MainAxisAlignment.SPACE_BETWEEN)],
