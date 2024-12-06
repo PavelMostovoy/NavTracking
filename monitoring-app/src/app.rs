4d38fd47edb781792d52a9ce06583859e05db3e4
p-mostovoi <145673281+p-mostovoi@users.noreply.github.com>
2024-12-06 17:15:33 +0100

    fixing app run and initial page change

--- monitoring-app/src/app.rs
+++ monitoring-app/src/app.rs
@@ -1,77 +1,96 @@
 #![allow(non_snake_case)]
 
+use dioxus::html::r#use;
 use dioxus::prelude::*;
-use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
+use dioxus_logger::tracing::{info, warn, Level};
+use gloo_utils::document;
+use leaflet::{Map, MapOptions, Marker, TileLayer};
+use web_sys::{wasm_bindgen::JsCast, HtmlElement};
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+#[derive(Clone)]
+struct MyContext {
+    info: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+#[derive(Props, Clone, PartialEq)]
+struct MapProps {
+    text: String,
+    #[props(optional)]
+    size: i32,
+}
+impl MapProps {
+    fn text(&mut self) {
+        self.text = "Updated Button".to_string();
+    }
+}
+
+
+#[component]
+fn my_button(mut props: MapProps) -> Element {
+    let context = use_context::<Signal<MyContext>>();
+    let mut count = use_signal(|| 0);
+    let mut value = use_signal(|| props.text.clone());
+    if props.size > 0 {
+        rsx! {
+       button{
+                onclick : move |event|{
+                    info!("Button clicked");
+                },
+                "{props.text} # {props.size}"}
+   }
+    } else {
+        rsx! {
+       button{onclick : move |_|{
+                count += 1;
+                props.text();
+                value.set(props.text.clone());
+        },
+                "{value} : {count} {context().info}"
+        }
+   }
+    }
+}
+
+
+#[component]
+fn MyMap() -> Element {
+    use_effect(move || {
+        const GREENWICH_MERIDIAN: (f64, f64) = (42.477806, 3.101472);
+
+        let container: HtmlElement = document()
+            .get_element_by_id("map")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+
+        let map = Map::new_with_element(&container, &MapOptions::default());
+
+        TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").add_to(&map);
+
+        map.set_view(&GREENWICH_MERIDIAN.into(), 14.0);
+
+        Marker::new(&GREENWICH_MERIDIAN.into()).add_to(&map);
+    });
+
+    rsx! {
+        div {
+            "style": "width:200px; height:200px;",
+            id: "map"
+        }
+    }
 }
 
 pub fn App() -> Element {
-    let mut name = use_signal(|| String::new());
-    let mut greet_msg = use_signal(|| String::new());
-
-    let greet = move |_: FormEvent| async move {
-        if name.read().is_empty() {
-            return;
-        }
-
-        let name = name.read();
-        let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &*name }).unwrap();
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        let new_msg = invoke("greet", args).await.as_string().unwrap();
-        greet_msg.set(new_msg);
-    };
-
+    use_context_provider(|| Signal::new(MyContext { info: "default text".to_string() }));
     rsx! {
-        link { rel: "stylesheet", href: "styles.css" }
-        main {
-            class: "container",
-            h1 { "Welcome to Tauri + Dioxus" }
-
-            div {
-                class: "row",
-                a {
-                    href: "https://tauri.app",
-                    target: "_blank",
-                    img {
-                        src: "/tauri.svg",
-                        class: "logo tauri",
-                         alt: "Tauri logo"
-                    }
-                }
-                a {
-                    href: "https://dioxuslabs.com/",
-                    target: "_blank",
-                    img {
-                        src: "/dioxus.png",
-                        class: "logo dioxus",
-                        alt: "Dioxus logo"
-                    }
-                }
-            }
-            p { "Click on the Tauri and Dioxus logos to learn more." }
-
-            form {
-                class: "row",
-                onsubmit: greet,
-                input {
-                    id: "greet-input",
-                    placeholder: "Enter a name...",
-                    value: "{name}",
-                    oninput: move |event| name.set(event.value())
-                }
-                button { r#type: "submit", "Greet" }
-            }
-            p { "{greet_msg}" }
+        div {
+            "style": "width:100px; height:100px; background-color: red",
+            p{"inner"}
         }
+        button {onclick: move |_| {}, "Button one"},
+        // MyMap{}
+        p{"Hi There !"}
+        my_button{text: "hello"}
+        my_button{text: "another", size:16}
     }
 }
--- monitoring-app/src/main.rs
+++ monitoring-app/src/main.rs
@@ -2,9 +2,11 @@ mod app;
 
 use app::App;
 use dioxus::prelude::*;
-use dioxus_logger::tracing::Level;
+use dioxus_logger::tracing::{info, Level};
 
 fn main() {
+    // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
     launch(App);
 }
