1199f51e17a26d363fa1a68f31addc3da91c3adb
p-mostovoi <145673281+p-mostovoi@users.noreply.github.com>
2024-12-13 13:44:21 +0100

    server port changed

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -50,7 +50,7 @@ async fn main() {
     println!("Pinged your database. Successfully connected to MongoDB!");
 
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3311").await.unwrap();
 
     axum::serve(listener, app(client))
         .await
