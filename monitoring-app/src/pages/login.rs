ccc75ff6a1282dd274f3646b16bbf0280efe9d0c
p-mostovoi <145673281+p-mostovoi@users.noreply.github.com>
2024-12-17 16:04:44 +0100

    login page updated as temporrary solution

--- monitoring-app/src/pages/login.rs
+++ monitoring-app/src/pages/login.rs
@@ -1,8 +1,8 @@
 #![allow(non_snake_case)]
 
+use std::collections::HashMap;
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info};
-
 use crate::pages::common::BackToLanding;
 
 #[derive(Clone, Debug)]
@@ -45,23 +45,93 @@ fn PasswordField() -> Element {
 
 #[component]
 fn Submit() -> Element {
+    let mut response = use_signal(|| String::from("..."));
+
+
+    let log_in = move |_| {
+        spawn(async move {
+            let mut data = HashMap::new();
+            data.insert("user_name", "admin");
+            data.insert("password", "password");
+
+
+            let client = reqwest::Client::new()
+                .post("https://api.mostovoi.org/hash")
+                .json(&data)
+                .basic_auth("admin", Some("admin123"))
+                .send()
+                .await;
+
+            match client {
+                Ok(data) => {
+                    response.set(String::from(data.text().await.unwrap_or(String::from("nothing received"))));
+                }
+                Err(err) => {
+                    log::info!("Request failed with error: {err:?}")
+                }
+            }
+
+        }
+        );
+    };
+
+
     let mut context = use_context::<Signal<LoginInfo>>();
-    rsx! {div{
-            button {
-            onclick: move |event| {
-                // let actual = context().logged_in;
-                context.write().logged_in = !context().logged_in ;
-                info!("Submit button pressed : {:?}", context());
-            },
+
+    if context().username.is_empty() | context().password.is_empty() {
+        rsx! {div{
+            button {disabled:true,
             "Login"
             }
+    }
+    }
+    } else {
+        rsx! {div{
+            button {
+            onclick: move |event| {
+                context.write().logged_in = !context().logged_in ;
+                info!("Submit button pressed : {:?}", context());
+                spawn(async move {
+                        let mut data = HashMap::new();
+                        data.insert("user_name", "admin");
+                        data.insert("password", "password");
+
+
+                        let client = reqwest::Client::new()
+                        .post("https://api.mostovoi.org/hash")
+                        .json(&data)
+                        .basic_auth("admin", Some("admin123"))
+                        .send()
+                       .await;
+
+            match client {
+                Ok(data) => {
+                     log::info!("Request successful")
+                }
+                Err(err) => {
+                    log::info!("Request failed with error: {err:?}")
+                }
+            }
+
+                    });
+
+
+            },
+            "Login"
+            },
+        button {onclick: post_request,
+            "Post Request"},
+
+        // button{
+        // onclick: log_in, "Response: {response}"}
+    }
     }
     }
 }
 
 
 pub fn Login() -> Element {
-    use_context_provider(||Signal::new(LoginInfo{
+    use_context_provider(|| Signal::new(LoginInfo {
         username: "".to_string(),
         password: "".to_string(),
         logged_in: false,
