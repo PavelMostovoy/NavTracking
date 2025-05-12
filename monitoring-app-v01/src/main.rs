fb76957eb4b0e0355896ef839b9ae5494ef8ffcb
Pavel Mostovoy <pavel.mostovoy@gmail.com>
2025-05-12 16:56:45 +0200

    Tracker selection added

--- monitoring-app-v01/src/main.rs
+++ monitoring-app-v01/src/main.rs
@@ -16,13 +16,17 @@ pub struct TrackerResult {
     pub data: Vec<crate::pages::selector::SimplifiedData>,
 }
 
-#[derive(Serialize)]
+#[derive(Debug,Serialize, Clone)]
 struct TrackerPayload {
     tracker_id: String,
     tracker_name: String,
 }
 
-
+#[derive(Debug, Clone)]
+pub struct SelectedTracker {
+    tracker_name: String,
+    tracker_id: String,
+}
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -37,6 +41,9 @@ fn App() -> Element {
     use_context_provider(|| Signal::new(
         TrackerResponse{ result: TrackerResult{ tracker_name: "".to_string(), data: vec![] }}
     ));
+    use_context_provider(|| Signal::new( 
+        SelectedTracker{ tracker_name: "".to_string(), tracker_id: "".to_string() }));
+
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
--- monitoring-app-v01/src/pages/selector.rs
+++ monitoring-app-v01/src/pages/selector.rs
@@ -1,32 +1,32 @@
 use crate::pages::Route;
+use crate::{SelectedTracker, TrackerPayload, TrackerResponse, TrackerResult};
 use dioxus::prelude::*;
 use dioxus_logger::tracing::info;
 use reqwest::Client;
+use serde::__private::de::Content::String;
 use serde::{Deserialize, Serialize};
-use crate::{TrackerPayload, TrackerResponse, TrackerResult};
+use std::thread::Scope;
 
 #[component]
 pub(crate) fn Selector(id: i32) -> Element {
-    let mut context = use_context::<Signal<TrackerResponse>>();
+    let selected_tracker = use_context::<Signal<SelectedTracker>>();
+    let context = use_context::<Signal<TrackerResponse>>();
+
     rsx! {
         div {
             id: "selector",
-            h1 { "Selection page" }
+            h3 { "Tracker and Data selection" }
+            div {
+                DropdownSelector {}
+            }
             button {
-            onclick: move |_| {
-                spawn(async move {
-                    context.write().result = send_tracker_request()
-                        .await.unwrap_or(TrackerResponse {result: TrackerResult {tracker_name: "".to_string(), data: vec![]}})
-                        .result;
-                    println!("{:?}", context().result);
-                });
-            },
-            "Get coordinates"
-        }
+                onclick: move |_| {
+                },
+                "Select Date"
+            }
         }
     }
 }
-
 #[derive(Debug, Deserialize, Clone)]
 pub struct SimplifiedData {
     pub lat: i32,
@@ -34,20 +34,82 @@ pub struct SimplifiedData {
     pub time: u32,
 }
 
-
-pub async fn send_tracker_request() -> Result<TrackerResponse, reqwest::Error> {
+pub async fn send_tracker_request(
+    tracker_payload: TrackerPayload,
+) -> Result<TrackerResponse, reqwest::Error> {
     let client = Client::new();
-    let payload = TrackerPayload {
-        tracker_id: "70b3d57ed00653c8".to_string(),
-        tracker_name: "hellteck Tracker 1".to_string(),
-    };
-
     let res = client
         .post("https://api.mostovoi.org/get_single_track")
-        .json(&payload)
+        .json(&tracker_payload)
         .send()
         .await?;
 
     let tracker_data = res.json::<TrackerResponse>().await?;
     Ok(tracker_data)
+}
+#[component]
+fn DropdownSelector() -> Element {
+    let mut selected_tracker = use_context::<Signal<SelectedTracker>>();
+    let tracker_data = use_context::<Signal<TrackerResponse>>();
+
+    let options = vec![
+        ("70b3d57ed00653c8", "hellteck Tracker 1"),
+        ("70b3d57ed00653c7", "hellteck Tracker 2"),
+    ];
+
+    rsx! {
+        div {
+            label { "Select sail Number: " }
+            select {
+                onchange: move |event| {
+                    let tracker_id = event.value();
+                    let tracker_name = options.iter()
+                    .find(|x| x.0 == tracker_id)
+                    .map(|x| x.1.to_string())
+                    .unwrap_or("Unknown".to_string());
+
+                    selected_tracker.set(SelectedTracker {
+                        tracker_id,
+                        tracker_name,
+                    });
+                   let payload = TrackerPayload {
+                            tracker_id: selected_tracker.read().tracker_id.clone(),
+                            tracker_name: selected_tracker.read().tracker_name.clone(),
+                        };
+                    let mut context = tracker_data.clone();
+
+                    spawn(async move {
+                        let response = send_tracker_request(payload)
+                            .await
+                            .unwrap_or(TrackerResponse {
+                                result: TrackerResult {
+                                    tracker_name: "".to_string(),
+                                    data: vec![],
+                                }
+                            });
+
+                        context.write().result = response.result;
+                        println!("Result: {:?}", context.read().result);
+
+                    });
+                },
+                option {
+                    value: "",
+                    disabled: true,
+                    selected: selected_tracker.read().tracker_id.is_empty(),
+                    "Select a tracker..."
+                }
+
+                for (id, name) in options.iter() {
+                    option {
+                        value: "{id}",
+                         selected: *id == selected_tracker.read().tracker_id,
+                        "{name}",
+                    }
+                }
+            }
+
+            p { "Debug Selected: ID = {selected_tracker.read().tracker_id}, Name = {selected_tracker.read().tracker_name}" }
+        }
+    }
 }
